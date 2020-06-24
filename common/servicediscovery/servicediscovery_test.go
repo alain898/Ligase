a0e618885cde8dbaf697947427d3a36e3d75ebf7
Shuang Zhou <shuang.zhou@maxent-inc.com>
2020-06-25 00:41:49 +0800

    fix bug

--- common/servicediscovery/servicediscovery_test.go
+++ common/servicediscovery/servicediscovery_test.go
@@ -3,13 +3,50 @@ package servicediscovery
 import (
 	"fmt"
 	"github.com/finogeeks/ligase/skunkworks/log"
+	"sync"
 	"testing"
 	"time"
 )
 
+type LocalLocker struct {
+	locker sync.RWMutex
+}
+
+func (wl *LocalLocker) Lock() {
+	wl.locker.Lock()
+}
+
+func (wl *LocalLocker) UnLock() {
+	wl.locker.Unlock()
+}
+
+func NewLocalLocker() Locker {
+	return new(LocalLocker)
+}
+
+var lock = NewLocalLocker()
+
+func doTest() {
+	servers := []string{"127.0.0.1:2181"}
+	client, err := NewDSClient(servers, "/servicediscovery", 10, &lock)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	if topic, err := client.Register("service1", ""); err == nil {
+		log.Infof(fmt.Sprintf("topic[%s]", topic))
+	}
+
+	for {
+		time.Sleep(time.Second)
+		log.Infof("tick")
+	}
+}
+
 func TestServiceDiscovery(t *testing.T) {
 	servers := []string{"127.0.0.1:2181"}
-	client, err := NewDSClient(servers, "/servicediscovery", 10, nil)
+	client, err := NewDSClient(servers, "/servicediscovery", 10, &lock)
 	if err != nil {
 		panic(err)
 	}
@@ -22,14 +59,9 @@ func TestServiceDiscovery(t *testing.T) {
 		log.Errorf("err[%+v]", err)
 	}
 
-	if topic, err := client.Register("service1", ""); err == nil {
-		log.Infof(fmt.Sprintf("topic[%s]", topic))
-	}
-	if topic, err := client.Register("service1", ""); err == nil {
-		log.Infof(fmt.Sprintf("topic[%s]", topic))
-	}
-	if topic, err := client.Register("service1", ""); err == nil {
-		log.Infof(fmt.Sprintf("topic[%s]", topic))
+	instances := 10
+	for i := 0; i < instances; i++ {
+		go doTest()
 	}
 
 	for {
